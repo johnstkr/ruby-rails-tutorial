26f93d6db665265476cbe5983917c3e474140f81
Ray Bullard <hello@raybullard.com>
2019-10-25 22:38:08 -0700

    Add testing

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
   get 'static_pages/home'
   get 'static_pages/help'
+  get 'static_pages/about'
   get 'users/new'
   get 'welcome/index'
 
-  root 'welcome#index'
+  root 'static_pages#home'
 end
