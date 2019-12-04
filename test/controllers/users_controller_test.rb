96043787c2f9c058af1a7a0dc5344ce04523a4f6
Ray Bullard <hello@raybullard.com>
2019-12-04 00:01:13 -0800

    Fix bundle file for heroku deploy

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -2,11 +2,8 @@ require 'test_helper'
 
 class UsersControllerTest < ActionDispatch::IntegrationTest
 
-end
   test "should get sign up" do
     get signup_path
-  test "should get new" do
-    get users_new_url
     assert_response :success
   end
 
